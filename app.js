a0ea4ec885fc677d9117b5ac74216fcc3ecb41a8
kaafivikrant <iamvikrant1@gmail.com>
2024-03-29 02:17:27 +0530

    bro

--- app.js
+++ app.js
@@ -1,14 +1,49 @@
-const http = require('node:http');
-const fs = require('node:fs');
+// const http = require('node:http');
+// const fs = require('node:fs');
 
-const server = http.createServer();
+// const server = http.createServer();
 
-server.on("request", (request, response) => {
-    const result = fs.readFileSync("./text.txt");
-    response.setHeader("Content-Type", "text/plain");
-    response.end(result);
+// server.on("request", (request, response) => {
+//     const result = fs.readFileSync("./text.txt");
+//     response.setHeader("Content-Type", "text/plain");
+//     response.end(result);
+// })
+
+// server.listen(4080, "127.0.0.1", () => {
+//     console.log("Server has started on", server.address())
+// })
+
+const express = require("express");
+const app = express();
+
+app.get("/", (request, response) => {
+    console.log("1")
+    response.send("esss wwdddwrld! !");
+});
+
+app.get("/add", (request, response) => {
+    response.send("Hello, 2! !");
+});
+
+app.listen(3000, () => {
+    console.log("Server has started on 3000")
 })
 
-server.listen(4080, "127.0.0.1", () => {
-    console.log("Server has started on", server.address())
+const EventEmitter = require("events");
+
+class Emitter extends EventEmitter {}
+
+const myE = new Emitter();
+
+// Create only one myE.on for all events
+
+
+myE.on('eventName', (event) => {
+  console.log(`Received event: ${event}`);
+});
+
+const arr = ['foo', 'woo', 'doo'];
+
+arr.forEach((item) => {
+    myE.emit('eventName', item);
 })
