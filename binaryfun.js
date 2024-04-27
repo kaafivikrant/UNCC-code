d2a40e4699dce669fe6ff507778e760f8cee7d18
kaafivikrant <iamvikrant1@gmail.com>
2024-04-28 00:04:56 +0530

    d

--- binaryfun.js
+++ binaryfun.js
@@ -1,31 +1,46 @@
-// var bnd = "1011";
-// bnd = bnd.split("");
-
-// console.log(bnd)
-
 function pow2(n) {
     return 2 << (n-1);
 }
 
-function BN(bnd){
-    // bnd = bnd.split(""); // ~ 1.7
-    bnd = Array.from(bnd, Number); // ~ 1.3
-    // bnd = [...bnd]; // ~ 1.75
+// function BN(bnd){ // ~621 ms
+//     // bnd = bnd.split(""); // ~ 1.7s
+//     bnd = Array.from(bnd, Number); // ~ 1.3s
+//     // bnd = [...bnd]; // ~ 1.75s
+//     let sum = 0;
+//     for(let i=bnd.length-1, sum = 0 , j = 0; i>=0; i--) { // ~ 680ms
+//         // sum += bnd[i] * Math.pow(2, j++); // ~ 1.3s
+//         sum += bnd[i] * pow2(j++);
+//     }
+
+//     /* ~ 684ms
+//         var len = bnd.length;
+//         var j = 0;
+//         while(len--){
+//             sum += bnd[len] * pow2(j++);
+//         }
+//     */
+
+//     return sum % 1 == 0? sum: "Invalid Binary Number";
+// }
+
+// function BN(bnd) { // 374ms
+//     let sum = 0, j = 0;
+//     for (let i = bnd.length - 1; i >= 0; i--) {
+//       sum += parseInt(bnd[i]) * pow2(j++);
+//     }
+//     return sum % 2 === 0 ? sum : "Invalid Binary Number";
+// }
+
+function BN(bnd){ // 185ms
     let sum = 0;
-    for(let i=bnd.length-1, sum = 0 , j = 0; i>=0; i--) { // ~ 680ms
-        // sum += bnd[i] * Math.pow(2, j++); // ~ 1.3s
-        sum += bnd[i] * pow2(j++);
-    }
-
-    /* ~ 684ms
-        var len = bnd.length;
-        var j = 0;
-        while(len--){
-            sum += bnd[len] * pow2(j++);
+    for (let i = bnd.length - 1, j = 0; i >= 0; i--, j++) {
+        if (bnd[i] === '1') {
+            sum += pow2(j);
+        } else if (bnd[i] !== '0') {
+            return "Invalid Binary Number";
         }
-    */
-
-    return sum % 1 == 0? sum: "Invalid Binary Number";
+    }
+    return sum;
 }
 
 
